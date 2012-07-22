882710e96fd556aa80d2837f5ca8c7b6bf9829a0
Lena Maria Schimmel <lena@lena-m70.(none)>
2012-07-22 09:01:59 +0200

    Removed the inner loops and extremely shortened it by the way.

--- projects/adcTest/adcTest.c
+++ projects/adcTest/adcTest.c
@@ -41,32 +41,6 @@ void convert(void)
 	ADCSRA  |= (1 << ADIF);					// Clear ADC Conversion Interrupt Flag
 }
 
-// Kernel to filter 2k at 74400 sampling rate
-float kernelFloat[] =
-{
-0.0008317398933697197,
--0.0005100205134740197,
--0.0032464298292837978,
--0.007856636732330745,
--0.012707626321608483,
--0.014618010079141791,
--0.010775581557413483,
--0.0008204973810931881,
-0.012309600197781309,
-0.02347120682912779,
-0.02784451098813323,
-0.02347120682912779,
-0.012309600197781309,
--0.0008204973810931881,
--0.01077558155741349,
--0.014618010079141793,
--0.012707626321608486,
--0.007856636732330747,
--0.0032464298292837995,
--0.0005100205134740197,
-0.0008317398933697197
-};
-
 void init_output()
 {
 	// Prepare registers to output something to the speaker
@@ -111,23 +85,12 @@ void textcolor(int attr, int fg, int bg)
 	l_bg = bg;
 }
 
-int kernelInt[21];
 
 PROCESS(adc_test_process, "adc test process");
 AUTOSTART_PROCESSES(&adc_test_process);
 
 PROCESS_THREAD(adc_test_process, ev, data)
 {
-	int i,n; // generic loop indices
-	int buf[512]; // buffer for original sound samples
-	int out[512]; // buffer for processed sound samples	
-	int avg; // variable to hold some average value (is (re-)used in multiple ways)
-	long int sum; // variable to hold some sum (is (re-)used in multiple ways)
- 	int nsi;
-	int cnt;
-	int last;
-	long int tmp;
-	
 
 	PROCESS_BEGIN();
 
@@ -145,111 +108,62 @@ PROCESS_THREAD(adc_test_process, ev, data)
 //	printf("##############dzdsfcdcuhlzceglirztzehcuilettrzcu####### adc_test_process talking now ###############\n");
 //	printf("##############dzdsfcdcuhlzceglizcuilehutzjjcu####### adc_test_process talking now ###############\n");
 
-	// Convert the float kernel to integer values for faster computation
-	for(i = 0; i < 21; i++)
-		kernelInt[i] = (int)(kernelFloat[i] * 100000.0f);
 	
+	int i; // generic loop index
+	static int last;
+	static int tmp;
+	static int np[10];
+	static int npi = 0;	
+
+	static int ringBuf[100];
+
+	memset(ringBuf, 0, sizeof(int) * 100);
+	static int ringIndex = 0;
+
+	static long int backSum = 0; // Summe der letzten 100 samples 
+
+	static long int t = 0;
+
 	// Main loop
 	while(1) 
 	{
-		sum = 0;		
-		for(i = 0; i < 512; i++)
+		t ++;
+		convert();
+		backSum -= (long int)ringBuf[ringIndex];
+		ringBuf[ringIndex] = result;
+		backSum += (long int)result;
+		ringIndex = (ringIndex + 1) % 100;
+
+		tmp = result - backSum / 100;
+//		printf("ri: %i\tv+: %i\tv0: %i\tbs:%i\n", ringIndex, result, tmp, (int)backSum);
+		
+
+		if((last < 0 && tmp > 0) || (last > 0 && tmp < 0))
 		{
-			convert();
-			buf[i] = result;
-			sum += result;
+			np[npi] = t;
+			npi = (npi + 1) % 10;
+			// Gesamtdauer der letzten 3 Perioden. Idealerweise wäre das 75.
+			int dif = t - np[(npi+3) % 10];
+			if(dif >= 8 && dif <= 11)
+			{
+				leds_on(LEDS_GREEN);
+				textcolor(RESET, GREEN, BLACK);	
+			}
+			else
+			{
+				leds_off(LEDS_GREEN);
+				textcolor(RESET, RED, BLACK);	
+			}
+			printf("%i\t", dif);
 		}
-
-		// avg = durchschnittlicher Level ( = Gleichspannungsanteil)
-		avg = sum / 512;	
-		sum = 0;	
-
-		nsi = -1;
-
-		int np[500];
-		int npi = 0;		
-
-		for(i = 10; i < 502; i++)
-		{
-			tmp = 0;
-			
-			//for(n = -10; n <= 10; n++)
-			//	tmp += (long int)(buf[i+n]-avg) * kernelInt[n]; 
-			//sum += abs(tmp);
-			//out[i] = tmp / 30;
-//			printf("%i, %i\n", buf[i], out[i]);
-			tmp = buf[i] - avg;
-			out[i] = tmp;
-			if((last < 0 && tmp > 0) || (last > 0 && tmp < 0))
-				np[npi++] = i;
-			last = tmp;
-		}
-
+		last = tmp;
 
 		// Number of "nulldurchgänge" which had the right distance to the previous one.
 		int goodCount = 0;
-		int badCount = 0;
 
-		for(i = 6; i < npi; i++)
-		{	
-			// Gesamtdauer der letzten 3 Perioden. Idealerweise wäre das 75.
-			int dif = np[i] - np[i-6];
-			if(dif < 70 || dif > 76)
-			{
-				badCount++;
-			//	textcolor(BRIGHT, RED, BLACK);	
-			//	printf("%i ", dif);
-			}
-			else
-			{
-				goodCount++;
-			//	textcolor(RESET, GREEN, BLACK);	
-			//	printf("%i ", dif);
-			}
-		}
+		
 
-		if(goodCount * 2 > badCount)
-		{
-			leds_on(LEDS_GREEN);
-			textcolor(RESET, GREEN, BLACK);	
-		}
-		else
-		{
-			leds_off(LEDS_GREEN);
-			textcolor(RESET, RED, BLACK);	
-		}
-
-
-
-		// random output, sometimes
-/*
-		if(goodCount % 20 == 0)
-			for(i = 10; i < 502; i++)
-				printf("%i, %i\n", buf[i], out[i]);
-*/
-
-		//printf("\nG: %i, B: %i\n", goodCount, badCount);
-		printf("\n%i", goodCount);
-		textcolor(RESET, WHITE, BLACK);	
-
-/*			{
-				goodCount--;
-				if(goodCount < 0)
-				{
-					leds_off(LEDS_GREEN);
-					goodCount = 0;
-				}
-			}
-			else
-			{
-				goodCount ++;
-				if(goodCount > 4)
-					leds_on(LEDS_GREEN);
-			}
-*/
-		//}
-		// avg = durchschnittliche Amplitude (nach abs)
-		avg = (int)(sum/492);
+//		printf("\n%i", goodCount);
 		
 		PROCESS_PAUSE();
 	}
